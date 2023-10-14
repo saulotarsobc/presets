a63d27fb75e7fbaf8d7dd4980adf088b4c7753eb
Saulo Costa <saulotarsobc@gmail.com>
2023-10-14 11:50:07 -0300

    :fire:

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log(12345)
+const datalist = document.querySelector("datalist");
+
+const pessoas = ["Aline", "Jaqueline", "Geise", "Saulo", "Filipe", "Sargio", "Sarah", "Rose", "Jhonatas", "Zilene", "Paulo H", "Jeferson", "Neia"]
+
+pessoas.forEach(pessoa => {
+    datalist.innerHTML += `<option value="${pessoa}"/>`;
+});
